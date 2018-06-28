a33ae6dede8ccc57cd059f6d6131fda4570ac5f5
pronthida <t.pornthida5688@gmail.com>
2018-06-28 16:39:12 +0700

    Finish June28

--- app/src/main/java/masterung/androidthai/in/th/srrufriend/fragment/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/srrufriend/fragment/MainFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import masterung.androidthai.in.th.srrufriend.R;
 
@@ -17,7 +18,20 @@ public class MainFragment extends Fragment{
         super.onActivityCreated(savedInstanceState);
 
 //        Register Controller
+        TextView textView = getView().findViewById(R.id.txtNewRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+//                  Replace Fragment
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment,new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
     }   // Main Method
 
     @Nullable
--- app/src/main/java/masterung/androidthai/in/th/srrufriend/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/srrufriend/fragment/RegisterFragment.java
@@ -1,16 +1,90 @@
 package masterung.androidthai.in.th.srrufriend.fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import masterung.androidthai.in.th.srrufriend.MainActivity;
 import masterung.androidthai.in.th.srrufriend.R;
 
 public class RegisterFragment extends Fragment{
+
+    private ImageView imageView;
+    private Uri uri;
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+ //         Create Toolbar
+        createToolbar();
+
+ //         Avata Controller
+        ImageView imageView = getView().findViewById(R.id.imvAvata);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Choose App"),1);
+
+            }
+        });
+
+    }   // Main Method
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK){
+
+            uri = data.getData();
+
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+
+
+        }   // if
+
+    }   // Result
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity)getActivity()).getSupportActionBar().setTitle("Register");
+
+        ((MainActivity)getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+    }
+
 
     @Nullable
     @Override
